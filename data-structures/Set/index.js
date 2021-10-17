a49b951ab3fbdfa44a858ac2e9890d966ec9f7e4
André Rodrigues <andre.rodrigues98@outlook.com>
2021-10-17 11:25:47 -0300

    adding base to Set

---
+++ data-structures/Set/index.js
@@ -0,0 +1,63 @@
+class Set {
+    constructor() {
+        this.items = {};
+    }
+
+    add(element) {
+        if (!this.has(element)) {
+            this.items[element] = element;
+            return true;
+        }
+
+        return false;
+    }
+
+    delete(element) {
+        if (this.has(element)) {
+            delete this.items[element];
+            return true;
+        }
+
+        return false;
+    }
+
+    has(element) {
+        return element in this.items;
+    }
+
+    clear() {
+        this.items = {};
+    }
+
+    size() {
+        return Object.keys(this.items).length;
+    }
+
+    sizeLegacy() {
+        let count = 0;
+        for (let key in this.items) {
+            if (this.items.hasOwnProperty(key)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    values() {
+        return Object.values(this.items);
+    }
+
+    valuesLegacy() {
+        let values = [];
+        for (let key in this.items) {
+            if (this.items.hasOwnProperty(key)) {
+                values.push(key);
+            }
+        }
+
+        return values;
+    }
+}
+
+module.exports = Set;
