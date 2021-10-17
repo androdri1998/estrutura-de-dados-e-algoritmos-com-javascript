b10b893568bd50904de1101c2509f0c32c6169c1
André Rodrigues <andre.rodrigues98@outlook.com>
2021-10-17 12:26:30 -0300

    adding Set

--- data-structures/Set/index.js
+++ data-structures/Set/index.js
@@ -58,6 +58,64 @@ class Set {
 
         return values;
     }
+
+    union(otherSet) {
+        const unionSet = new Set();
+        this.values().forEach(value => unionSet.add(value));
+        otherSet.values().forEach(value => unionSet.add(value));
+
+        return unionSet;
+    }
+
+    intersection(otherSet) {
+        const intersectionSet = new Set();
+        const values = this.values();
+        const otherValues = otherSet.values();
+        let biggestSet = values;
+        let smallestSet = otherValues;
+
+        if (otherValues.length - values.length > 0) {
+            biggestSet = otherValues;
+            smallestSet = values;
+        }
+
+        smallestSet.forEach((value) => {
+            if (biggestSet.includes(value)) {
+                intersectionSet.add(value);
+            }
+        });
+
+        return intersectionSet;
+    }
+
+    difference(otherSet) {
+        const differenceSet = new Set();
+        this.values().forEach((value) => {
+            if (!otherSet.has(value)) {
+                differenceSet.add(value);
+            }
+        });
+
+        return differenceSet;
+    }
+
+    isSubsetOf(otherSet) {
+        if (this.size() > otherSet.size()) {
+            return false;
+        }
+
+        let isSubset = true;
+        this.values().every(value => {
+            if (!otherSet.has(value)) {
+                isSubset = false;
+                return false;
+            }
+
+            return true;
+        });
+
+        return isSubset;
+    }
 }
 
 module.exports = Set;
--- data-structures/examples/Set/index.js
+++ data-structures/examples/Set/index.js
@@ -14,3 +14,32 @@ set.delete(1);
 console.log(set.values());
 set.delete(2);
 console.log(set.values());
+
+set.add(1);
+set.add(2);
+set.add(3);
+
+const setB = new Set();
+setB.add(2);
+setB.add(3);
+setB.add(4);
+setB.add(5);
+setB.add(6);
+
+const setC = new Set();
+setC.add(1);
+setC.add(2);
+setC.add(3);
+setC.add(4);
+
+const unionAB = set.union(setB);
+console.log(unionAB.values());
+const intersectioAB = set.intersection(setB);
+console.log(intersectioAB.values());
+const differenceAB = set.difference(setB);
+console.log(differenceAB.values());
+const differenceBA = setB.difference(set);
+console.log(differenceBA.values());
+
+console.log(set.isSubsetOf(setC));
+console.log(set.isSubsetOf(setB));
