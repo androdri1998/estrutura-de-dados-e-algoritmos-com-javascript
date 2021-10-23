748c2ce1beedd45f6c2bab80414c6542601e1428
André Rodrigues <andre.rodrigues98@outlook.com>
2021-10-23 08:29:43 -0300

    adding Hash table separate chaining

--- data-structures/HashTable/hash-table-separate-chaining.js
+++ data-structures/HashTable/hash-table-separate-chaining.js
@@ -41,11 +41,22 @@ class HashTableSeparateChaining {
     }
 
     remove(key) {
-        const hash = this.hashCode(key);
-        const valuePair = this.table[hash];
-        if (valuePair != null) {
-            delete this.table[hash];
-            return true;
+        const position = this.hashCode(key);
+        const linkedList = this.table[position];
+        if (linkedList != null && !linkedList.isEmpty()) {
+            let current = linkedList.getHead();
+            while (current != null) {
+                if (current.element.key === key) {
+                    linkedList.remove(current.element);
+                    if (linkedList.isEmpty()) {
+                        delete this.table[position];
+                    }
+
+                    return true;
+                }
+
+                current = current.next;
+            }
         }
 
         return false;
