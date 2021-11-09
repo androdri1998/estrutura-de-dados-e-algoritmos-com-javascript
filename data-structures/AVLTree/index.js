e5151cd8bc4b6750c7bfead2a65fe81c3098c6c2
André Rodrigues <andre.rodrigues98@outlook.com>
2021-11-09 09:59:02 -0300

    adding AVLTree

--- data-structures/AVLTree/index.js
+++ data-structures/AVLTree/index.js
@@ -1,4 +1,4 @@
-const { defaultCompare, Compare } = require('../BinarySearchTree/util');
+const { defaultCompare, Compare, BalanceFactor } = require('../BinarySearchTree/util');
 const Node = require('../BinarySearchTree/node');
 
 class AVLTree extends BinarySearchTree {
@@ -6,6 +6,126 @@ class AVLTree extends BinarySearchTree {
         super(compareFn);
         this.compareFn = compareFn;
         this.root = null;
+    }
+
+    getBalanceFactor(node) {
+        const heightDifference = this.getNodeHeight(node.left) - this.getBalanceFactor(node.right);
+
+        switch (heightDifference) {
+            case -2:
+                return BalanceFactor.UNBALANCED_RIGHT;
+            case -1:
+                return BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT;
+            case 1:
+                return BalanceFactor.SLIGHTLY_UNBALANCED_LEFT;
+            case 2:
+                return BalanceFactor.UNBALANCED_LEFT;
+            default:
+                return BalanceFactor.BALANCED;
+        }
+    }
+
+    rotationLL(node) {
+        const tmp = node.left;
+        node.left = tmp.right;
+        tmp.right = node;
+        return tmp;
+    }
+
+    rotationRR(node) {
+        const tmp = node.right;
+        node.right = tmp.left;
+        tmp.left = node;
+        return tmp;
+    }
+
+    rotationLR(node) {
+        node.left = this.rotationRR(node.left);
+        return this.rotationLL(node);
+    }
+
+    rotationRL(node) {
+        node.right = this.rotationLL(node.right);
+        return this.rotationRR(node);
+    }
+
+    insertNode(node, key) {
+        if (node == null) {
+            return new Node(key);
+        } else if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+            node.left = this.insertNode(node.left, key);
+        } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+            node.right = this.insertNode(node.right, key);
+        } else {
+            return node;
+        }
+
+        const balanceFactor = this.getBalanceFactor(node);
+        if (balanceFactor === BalanceFactor.UNBALANCED_LEFT) {
+            if (this.compareFn(key, node.left.key) === Compare.LESS_THAN) {
+                node = this.rotationLL(node);
+            } else {
+                return this.rotationLR(node);
+            }
+        }
+
+        if (balanceFactor === BalanceFactor.UNBALANCED_RIGHT) {
+            if (this.compareFn(key, node.right.key) === Compare.BIGGER_THAN) {
+                node = this.rotationRR(node);
+            } else {
+                return this.rotationRL(node);
+            }
+        }
+
+        return node;
+    }
+
+    insert(key) {
+        this.root = this.insertNode(this.root, key);
+    }
+
+    removeNode(node, key) {
+        node = super.removeNode(node, key);
+
+        if (node === null) {
+            return node;
+        }
+
+        const balanceFactor = this.getBalanceFactor(node);
+        if (balanceFactor === BalanceFactor.UNBALANCED_LEFT) {
+            const balanceFactorLeft = this.getBalanceFactor(node.left);
+            if (
+                balanceFactorLeft === BalanceFactor.BALANCED ||
+                balanceFactorLeft === BalanceFactor.SLIGHTLY_UNBALANCED_LEFT
+            ) {
+                return this.rotationLL(node);
+            }
+
+            if (balanceFactorLeft === BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT) {
+                return this.rotationLR(node.left);
+            }
+        }
+
+        if (balanceFactor === BalanceFactor.UNBALANCED_RIGHT) {
+            const balanceFactorRight = this.getBalanceFactor(node.right);
+
+            if (
+                balanceFactorRight === BalanceFactor.BALANCED ||
+                balanceFactorRight === BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT
+            ) {
+                return this.rotationRR(node);
+            }
+
+            if (balanceFactorRight === BalanceFactor.SLIGHTLY_UNBALANCED_LEFT) {
+                return this.rotationRL(node.right);
+            }
+        }
+
+        return node;
+    }
+
+    remove(key) {
+        this.root = this.removeNode(this.root, key);
     }
 }
 
--- data-structures/BinarySearchTree/util.js
+++ data-structures/BinarySearchTree/util.js
@@ -11,7 +11,16 @@ function defaultCompare(a, b) {
     return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
 }
 
+const BalanceFactor = {
+    UNBALANCED_RIGHT: 1,
+    SLIGHTLY_UNBALANCED_RIGHT: 2,
+    SLIGHTLY_UNBALANCED_LEFT: 4,
+    UNBALANCED_LEFT: 5,
+    BALANCED: 3,
+};
+
 module.exports = {
     Compare,
     defaultCompare,
+    BalanceFactor,
 }
