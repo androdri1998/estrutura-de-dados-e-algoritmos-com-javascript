96517b7518226e0033955a11d3cd4b40fb25ac67
André Rodrigues <andre.rodrigues98@outlook.com>
2022-01-09 13:20:05 -0300

    adding Bubble sort Algorithm

---
+++ algorithms/BubbleSort/index.js
@@ -0,0 +1,38 @@
+const { Compare, defaultCompare, swap, createNonSortedArray } = require('../../utils');
+
+const bubbleSort = (array, compareFn = defaultCompare) => {
+    const { length } = array;
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length - 1; j++) {
+            if (compareFn(array[j], array[j + 1]) === Compare.BIGGER_THAN) {
+                swap(array, j, j + 1);
+            }
+        }
+    }
+
+    return array;
+}
+
+let array = createNonSortedArray(5);
+console.log(array.join());
+array = bubbleSort(array);
+console.log(array.join());
+
+const modifiedBubbleSort = (array, compareFn = defaultCompare) => {
+    const { length } = array;
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length - 1 - i; j++) {
+            if (compareFn(array[j], array[j + 1]) === Compare.BIGGER_THAN) {
+                swap(array, j, j + 1);
+            }
+        }
+    }
+
+    return array;
+}
+
+let array2 = createNonSortedArray(5);
+console.log('------------------')
+console.log(array2.join());
+array2 = bubbleSort(array2);
+console.log(array2.join());
---
+++ utils/index.js
@@ -0,0 +1,34 @@
+const Compare = {
+    LESS_THAN: -1,
+    BIGGER_THAN: 1
+};
+
+function defaultCompare(a, b) {
+    if (a === b) {
+        return 0;
+    }
+
+    return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
+}
+
+function swap(array, a, b) {
+    const temp = array[a];
+    array[a] = array[b];
+    array[b] = temp;
+}
+
+const createNonSortedArray = (size) => {
+    const array = [];
+    for (let i = size; i > 0; i--) {
+        array.push(i);
+    }
+
+    return array;
+}
+
+module.exports = {
+    Compare,
+    defaultCompare,
+    swap,
+    createNonSortedArray,
+}
