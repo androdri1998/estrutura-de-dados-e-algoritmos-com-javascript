8dd41c90ad01e9397a131a0a2c0526134f40072b
André Rodrigues <andre.rodrigues98@outlook.com>
2022-03-01 11:06:25 -0300

    adding new tests with MinHeap and MaxHeap

--- data-structures/examples/MinHeap/index.js
+++ data-structures/examples/MinHeap/index.js
@@ -2,7 +2,7 @@ const MinHeap = require('../../MinHeap');
 
 const heap = new MinHeap();
 
-for (let i = 1; i < 10; i++) {
+for (let i = 10; i >= 1; i--) {
     heap.insert(i);
 }
 
@@ -10,5 +10,11 @@ console.log('Heap size', heap.size());
 console.log('Heap is empty', heap.isEmpty());
 console.log('Heap min value', heap.findMinimum());
 console.log('Heap', heap.heap);
+console.log('Extract', heap.extract());
+console.log('Extract', heap.extract());
+console.log('Extract', heap.extract());
+console.log('Extract', heap.extract());
+console.log('Extract', heap.extract());
+console.log('Extract', heap.extract());
 console.log('Extract', heap.extract());
 console.log('Heap', heap.heap);
